5678e9938feef502994c7ea630d6e60a47fe4097
Aleksa Marie Ocampo <156120021+aleksaocampo@users.noreply.github.com>
2025-09-28 09:04:22 +0000

    Walkthrough completed

---
+++ script.js
@@ -0,0 +1,38 @@
+// Get all needed DOM elements
+const form = document.getElementById("checkInForm"); // ID: checkInForm found in html
+const teamSelect = document.getElementById("teamSelect");
+const nameInput = document.getElementById("attendeeName");
+
+// track attendance
+let count = 0;
+const maxCount = 50;
+
+// Handle form submission using browser's event listeners
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  // get form values
+  const name = nameInput.value;
+  const team = teamSelect.value;
+  const teamName = teamSelect.selectedOptions[0].text;
+
+  console.log(name, team, teamName);
+
+  // increment count
+  count++;
+  console.log("Total check-ins: ", count);
+
+  // update progress bar
+  const percentage = Math.round((count / maxCount) * 100) + "%";
+  console.log(`Progress: ${percentage}`);
+
+  // update team counter
+  let teamCounter = document.getElementById(team + "Count");
+  teamCounter.textContent = parseInt(teamCounter.textContent) + 1;
+
+  // show welcome message
+  const message = `Welcome, ${name} from ${teamName}`;
+  console.log(message);
+
+  form.reset();
+});
